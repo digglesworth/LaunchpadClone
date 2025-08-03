a46a260a6e4411755e989340294360e9ae63150d
James Metcalf <jmetcalf1177@gmail.com>
2025-08-03 18:47:46 -0400

    Initial Commit

---
+++ LaunchpadClone/LaunchpadCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchpadCloneApp.swift
+//  LaunchpadClone
+//
+//  Created by James Metcalf on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchpadCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaunchpadCloneTests/LaunchpadCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchpadCloneTests.swift
+//  LaunchpadCloneTests
+//
+//  Created by James Metcalf on 8/3/25.
+//
+
+import Testing
+@testable import LaunchpadClone
+
+struct LaunchpadCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
